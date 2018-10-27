26cc0b018c8d616144ae422f55d84907789fd18a
arosaku <arosaku28@yahoo.co.jp>
2018-10-27 19:41:22 +0900

    toriaezu

--- index.js
+++ index.js
@@ -48,11 +48,11 @@ function handleEvent(event) {
   if (event.beacon.type === 'enter'){
     console.log('req.body.events');
 
-    client.multicast(['U4577fa37ae7f522822aae54146978712'], {
-      "text": event.message.text ,
-      "type" : 'text'
-      }
-    );
+    // client.multicast(['U4577fa37ae7f522822aae54146978712'], {
+    //   "text": event.message.text ,
+    //   "type" : 'text'
+    //   }
+    // );
   }
   if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null);
